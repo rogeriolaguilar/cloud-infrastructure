ac25868db92c6515675825ec2d208ce074f309a7
Rogério Libarino Aguilar <rogerio.l.aguilar@gmail.com>
2018-08-21 22:11:08 -0300

    organizing terraform files

--- terraform.sh
+++ terraform.sh
@@ -1,12 +1,15 @@
+TERRAFORM_FOLDER=terraform
+VAR_FILE=$TERRAFORM_FOLDER/terraform.tfvars
+
 case ${1} in
   "init" )
-    terraform init ;;
+    terraform init $TERRAFORM_FOLDER;;
   "plan" )
-    terraform plan -var-file=terraform.tfvars ;;
+    terraform plan -var-file=$VAR_FILE $TERRAFORM_FOLDER;;
   "apply" )
-    terraform apply -var-file=terraform.tfvars ;;
+    terraform apply -var-file=$VAR_FILE $TERRAFORM_FOLDER;;
   "destroy" )
-    terraform destroy -var-file=terraform.tfvars ;;
+    terraform destroy -var-file=$VAR_FILE $TERRAFORM_FOLDER;;
   * )
     echo "Invalid commad \"$1\"... try: init, plan, apply, destroy"
 esac
